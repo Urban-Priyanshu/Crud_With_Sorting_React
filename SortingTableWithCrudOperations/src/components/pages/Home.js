feccf340036aae885ad6197d2196fa48032c9e9f
Priyanshu Mishra <priyanshu.mishra@compunneldigital.com>
2022-06-27 23:27:32 +0530

    Crud27june2022

---
+++ SortingTableWithCrudOperations/src/components/pages/Home.js
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const [users, setUser] = useState([]); 
+  const [loaded, setLoaded] = useState(false);
+  const [order, setorder] = useState("ASC");
+
+  const loadUsers = async () => {
+    
+      const result = await axios.get("http://localhost:3003/users");
+      setUser(result.data);
+         
+  };
+
+  useEffect(async () => {
+    if(!loaded) {
+      await loadUsers();
+      setLoaded(true);
+    }
+  }, []);
+
+  
+  const deleteUser = async id => {
+    await axios.delete(`http://localhost:3003/users/${id}`);
+    loadUsers();
+  };
+
+  //   const onSortBy = (sortBy) => {
+  //     const tempUsers = sortUsers(sortBy);
+  //     setUser(tempUsers);
+  //   }
+
+  // const sortUsers = (sortByField) => {
+
+  //   return users.slice().sort((a, b) =>
+  //       a[sortByField]?.localeCompare(b[sortByField])
+  //     );
+
+  const onSortBy = (sortBy) => {
+    if(order === "ASC"){
+      const sorted = [...users].sort((a, b) => 
+      a[sortBy].toLowerCase() > b[sortBy].toLowerCase() ? 1 : -1
+      );
+      setUser(sorted);
+      setorder("DSC");
+    }
+    if(order === "DSC"){
+      const sorted = [...users].sort((a, b) => 
+      a[sortBy].toLowerCase() < b[sortBy].toLowerCase() ? 1 : -1
+      );
+      setUser(sorted);
+      setorder("ASC");
+    }
+
+  };
+
+  const Priyanshu = () => {
+    return(
+      <h2>sakaham</h2>
+    );
+  }
+
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>Home Page</h1>
+          <table class="table border shadow">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col" onClick={() => onSortBy('firstname')} >FirstName</th>
+              <th scope="col" onClick={() => onSortBy('lastname')}>LastName</th>
+              <th scope="col"  onClick={() => onSortBy('email')}>Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr>
+                <th scope="row">{index + 1}</th>
+                <td>{user.firstname}</td>
+                <td>{user.lastname}</td>
+                <td>{user.email}</td>
+                <td>
+                  <Link class="btn btn-primary mr-2" to={`/users/${user.id}`}>
+                    View
+                  </Link>
+                  <Link
+                    class="btn btn-outline-primary mr-2"
+                    to={`/users/edit/${user.id}`}
+                  >
+                    Edit
+                  </Link>
+                  <Link
+                    class="btn btn-danger"
+                    onClick={() => deleteUser(user.id)}
+                  >
+                    Delete
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
